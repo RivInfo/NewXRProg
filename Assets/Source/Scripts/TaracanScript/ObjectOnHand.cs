eaa36cd3f35946e6d9be02f95acc17cdf3471d82
Роман Иванов <romaiv99@gmail.com>
2022-11-19 20:26:18 +0300

    правки

---
+++ Assets/Source/Scripts/TaracanScript/ObjectOnHand.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ObjectOnHand : MonoBehaviour
+{
+    private XRGrabInteractable _xRGrabInteractable;
+
+    private void Start()
+    {
+        _xRGrabInteractable = GetComponent<XRGrabInteractable>();
+
+        //_xRGrabInteractable.
+    }
+}
