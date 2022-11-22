fe42e1c4634d4e0e941fb80e74ccd37649f171a0
Роман Иванов <romaiv99@gmail.com>
2022-11-23 01:27:52 +0300

    всякие апгрейды, если тару с пельменями в нутри высоко подкинуть, она будет колбаситься в воздухе, как только гравитация до неё дойдёт. Из за того что пельмени внутри кинематичные (будет фичей)

--- Assets/Source/Scripts/ActionObject/Pelmen.cs
+++ Assets/Source/Scripts/ActionObject/Pelmen.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider),typeof(Rigidbody))]
-public class Pelmen : MonoBehaviour
+public class Pelmen : SelectidOnHandObject
 {
     private bool _isActive = false;
 
@@ -14,13 +15,14 @@ public class Pelmen : MonoBehaviour
     {
         _colliders = GetComponentsInChildren<Collider>();
         _rigidbody = GetComponent<Rigidbody>();
-        SetColliderTrigger(true);
+        //SetColliderTrigger(true);
     }
 
     public void Activate()
     {
         _isActive = true;
         SetColliderTrigger(true);
+       // ConsoleTimeLog("pel");
     }
 
     public void Dectivate()
@@ -31,11 +33,29 @@ public class Pelmen : MonoBehaviour
         SetColliderTrigger(false);
     }
 
+    protected override void ObjectInHand(SelectEnterEventArgs args)
+    {
+        base.ObjectInHand(args);
+        Activate();
+    }
+
+    protected override void ObjectOutHand(SelectExitEventArgs args)
+    {
+        base.ObjectOutHand(args);
+        Dectivate();
+    }
+
     private void SetColliderTrigger(bool isTrigger)
     {
         foreach (var item in _colliders)
         {
             item.isTrigger = isTrigger;
         }
+    }
+
+
+    public void ConsoleTimeLog(string messege)
+    {
+        Debug.LogWarning(Time.time + " - " + messege);
     }
 }
--- Assets/Source/Scripts/Riv/TaraFromPelmen.cs
+++ Assets/Source/Scripts/Riv/TaraFromPelmen.cs
@@ -3,36 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class Weapon : SelectidOnHandObject
+public class TaraFromPelmen : SelectidOnHandObject
 {
-    private bool _isActive = false;
-
     private Collider[] _colliders;
 
     private void Start()
-    {
+    {       
         _colliders = GetComponentsInChildren<Collider>();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (_isActive && other.TryGetComponent(out Enemy enemy))
+        foreach(Collider collider in _colliders)
         {
-            enemy.TakeHit();
+            if (collider.gameObject.TryGetComponent(out Pelmen pelmen))
+            {
+                _interector.colliders.Remove(collider);
+            }
         }
     }
 
     public void Activate()
     {
-        //ConsoleTimeLog("Activate");
-        _isActive = true;
+        _colliders = GetComponentsInChildren<Collider>();
         SetColliderTrigger(true);
     }
 
     public void Dectivate()
     {
-        //ConsoleTimeLog("Dectivate");
-        _isActive = false;
         SetColliderTrigger(false);
     }
 
@@ -55,9 +49,4 @@ public class Weapon : SelectidOnHandObject
             item.isTrigger = isTrigger;
         }
     }
-
-    //public void ConsoleTimeLog(string messege)
-    //{
-    //    Debug.LogWarning(Time.time +" - " + messege);
-    //}
 }
